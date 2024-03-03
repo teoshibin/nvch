e3103ea181ec651c81934d2b7d6ae9a5ebfddda4
ShiBin Teo <62151367+teoshibin@users.noreply.github.com>
2024-03-03 23:40:48 +0000

    os specific server exclusion

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -5,20 +5,28 @@ local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+local os = require "custom.lib.os"
 local servers = {
   ["tsserver"] = {},
   ["clangd"] = {},
   ["powershell_es"] = {
-    bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services/"
+    install_condition = os.isWindows(),
+    bundle_path = vim.fn.stdpath "data" .. "/mason/packages/powershell-editor-services/",
   },
 }
 
 for lsp, mods in pairs(servers) do
+  local install = vim.tbl_get(mods, "install_condition")
+  if install ~= nil and not install then
+    goto continue
+  end
+
   local defaults = {
     on_attach = on_attach,
     capabilities = capabilities,
   }
   local opts = vim.tbl_deep_extend("force", defaults, mods)
   lspconfig[lsp].setup(opts)
-end
 
+  ::continue::
+end
