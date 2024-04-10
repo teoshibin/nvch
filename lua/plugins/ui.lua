97c72e092d83eb5f6e8a30a56df26b87af797524
ShiBin Teo <62151367+teoshibin@users.noreply.github.com>
2024-04-10 01:03:07 +0100

    feat: file explorer oil.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -106,4 +106,28 @@ return {
         "kevinhwang91/nvim-bqf",
         ft = "qf",
     },
+    {
+        -- file manager
+        "stevearc/oil.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {
+            delete_to_trash = true,
+            view_options = {
+                show_hidden = true,
+            },
+            float = {
+                padding = 5,
+                win_options = {
+                    winblend = 5,
+                },
+            },
+            keymaps = {
+                ["<leader>-"] = "actions.close",
+            },
+        },
+        cmd = "Oil",
+        keys = {
+            { "<leader>-", "<CMD>Oil<CR>", mode = "n", desc = "Toggle oil file manager" },
+        },
+    },
 }
