8084bd801606d5970e9ad96ee7362e4f99e13623
ShiBin Teo <62151367+teoshibin@users.noreply.github.com>
2024-04-30 00:02:34 +0100

    feat: obisidan WIP

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -77,6 +77,79 @@ return {
             pre_save_cmds = { "NvimTreeClose" },
         },
     },
+    {
+        "epwalsh/obsidian.nvim",
+        version = "*", -- recommended, use latest release instead of latest commit
+        lazy = true,
+        ft = "markdown",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        opts = {
+            workspaces = {
+                {
+                    name = "personal",
+                    path = "~/brain",
+                },
+            },
+            mappings = {
+                ["gf"] = {
+                    action = function()
+                        return require("obsidian").util.gf_passthrough()
+                    end,
+                    opts = { noremap = false, expr = true, buffer = true },
+                },
+                ["<leader>ic"] = {
+                    action = function()
+                        return require("obsidian").util.toggle_checkbox()
+                    end,
+                    opts = { buffer = true },
+                },
+                ["<leader>ia"] = {
+                    action = function()
+                        return require("obsidian").util.smart_action()
+                    end,
+                    opts = { buffer = true, expr = true },
+                },
+            },
+            ---@param title string|?
+            ---@return string
+            note_id_func = function(title)
+                -- Create note IDs in a Zettelkasten format with a date and a suffix.
+                -- In this case a note with the title 'My new note' will be given an ID that looks
+                -- like '2023-04-28_my-new-note.md'
+                local os_date = os.date("*t") -- Get table with year, month, day etc.
+                local formatted_date = string.format("%04d-%02d-%02d", os_date.year, os_date.month, os_date.day)
+
+                local suffix = ""
+                if title ~= nil then
+                    -- If title is given, transform it into a valid file name.
+                    suffix = title:gsub("[^%w%s-]", ""):gsub("%s+", "-"):lower() -- improved regex to handle non-alphanumeric and whitespace correctly
+                else
+                    -- If title is nil, just add 4 random uppercase letters to the suffix.
+                    math.randomseed(os.time()) -- Seed the random number generator to make it less predictable
+                    for _ = 1, 4 do
+                        suffix = suffix .. string.char(math.random(65, 90))
+                    end
+                end
+                return formatted_date .. "_" .. suffix .. ".md"
+            end,
+            attachments = {
+                img_folder = "assets/images",
+            },
+            templates = {
+                subdir = "templates",
+            },
+        },
+        -- config = function(_, opts)
+        --     local client = require("obsidian").get_client()
+        --     local map = require("mappings").map
+        --     map("n", "<leader>in", function()
+        --         local title = 
+        --         client.create_note({  })
+        --     end, { desc = "Obsidian create new notes" })
+        -- end,
+    },
     -- {
     --   -- TODO: https://github.com/smoka7/multicursors.nvim?tab=readme-ov-file
     --
